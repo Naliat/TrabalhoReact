a0b1dd932aab09d5c5878cc71435970fdcb007a9
Naliat <tailanoliveira584@gmail.com>
2025-01-28 23:24:34 -0300

    Alterações feitas pelo Tailan

--- livraria/App.jsx
+++ livraria/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
 import Cart from './src/pages/Cart';
 import Header from './components/Header';
+import NotFound from './pages/NotFound'; 
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/livro/:id" element={<ProductDetails />} />
         <Route path="/carrinho" element={<Cart />} />
+        <Route path="/notfound" element={<NotFound />} /> {/* Nova rota "notdofund" */}
+        <Route path="*" element={<NotFound />} /> {/* Rota "catch-all" */}
       </Routes>
     </Router>
   );
--- livraria/src/pages/NotFound.jsx
+++ livraria/src/pages/NotFound.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
+import dog1 from '../imgs/dog1.jpg';
+import dog2 from '../imgs/index';
+import dog3 from '../imgs/dog3.jpg';
 
 const NotFoundWrapper = styled.div`
   padding: 20px;
   text-align: center;
 `;
 
+const ErrorImage = styled.img`
+  max-width: 100%;
+  height: auto;
+  margin-top: 20px;
+`;
+
+const images = [dog1, dog2, dog3, dog1, dog2];
+
 function NotFound() {
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
   return (
     <NotFoundWrapper>
       <h1>404 - Página Não Encontrada</h1>
       <p>A página que você está procurando não existe.</p>
+      <ErrorImage src={randomImage} alt="Erro 404" />
     </NotFoundWrapper>
   );
 }
